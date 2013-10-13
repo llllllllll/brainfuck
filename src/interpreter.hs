05cf658533679cce8e1758234b9a5c83dc6b5130
llllllllll <joejev@gmail.com>
2013-10-13 03:02:24 -0400

    reworked some stuff.

--- src/interpreter.hs
+++ src/interpreter.hs
@@ -1,129 +1,136 @@
 -- Joe Jevnik
 -- 9.11.2013
--- Edited: 9.12.2013
+-- Edited: 13.10.2013
 
-import System.Environment
-import System.IO
-import Control.Monad
-import Control.Applicative
-import Data.Array
-import Data.Word
+import Control.Applicative ((<$>))
+import Control.Monad (void)
+import Data.Array.Unboxed (Array,(!),(//),listArray)
+import Data.Word (Word32)
+import System.Environment (getArgs)
+import Text.Read (readMaybe)
 
 -- Data type to hold the state of the program.
 data ProgState = ProgState { ptr     :: Int               -- Value Pointer
                            , vals    :: Array Int Word32  -- Values
                            , file    :: (String,String)   -- (consumed,left)
                            , loop_fl :: [(String,String)] -- (consumed,left)
-                           }
+                            }
 
--- Starts consuming characters from the ProgState file.
+-- |Starts consuming characters from the ProgState file.
 main :: IO ()
-main = do
-    args <- getArgs
-    consume_chars (parse_args args)
-    return ()
+main = void $ getArgs >>= parse_args >>=  consume_chars
 
 -- Parses the command line arguments to interperet the flags.
 parse_args :: [String]-> IO ProgState
-parse_args args = do
-    if head args == "-m"
+parse_args as =
+    if head as == "-m"
       then do
-          fl <-  readFile (args!!2) 
-          return (ProgState 0 (listArray (0,read (args!!1) - 1) (repeat 0)) 
-                                    ("",fl) [])
+          fl <- readFile $ as!!2
+          let ln=  case readMaybe $ as!!1 of
+                       Nothing -> error "'-m' expects and Int argument"
+                       Just n  -> n - 1
+          return ProgState { ptr     = 0
+                           , vals    = listArray (0,ln) (repeat 0)
+                           , file    = ("",fl)
+                           , loop_fl = []
+                           }
       else do
-          fl <- readFile (head args)
-          return (ProgState 0 (listArray (0,29999) (repeat 0))
-                                    ("",fl) [])
+          fl <- readFile $ head as
+          return ProgState { ptr     = 0
+                           , vals    = listArray (0,29999) (repeat 0)
+                           , file    = ("",fl)
+                           , loop_fl = []
+                           }
 
--- Parses the next character from the ProgState.
-consume_chars :: IO ProgState -> IO ProgState
-consume_chars iost = do
-    st <- iost
+-- |Parses the next character from the ProgState.
+consume_chars :: ProgState -> IO ProgState
+consume_chars st = do
     if (not $ null ((snd . file) st))
       then do
           let c = (toEnum . fromEnum) $ (head . snd . file) st
-          consume_chars $ parse_char c st
-      else return (ProgState 0 (listArray (0,29999) (repeat 0)) ("","") []) 
+          parse_char c st >>= consume_chars
+      else return (ProgState 0 (listArray (0,29999) (repeat 0)) ("","") [])
 
--- Parses a character into the proper function.
+-- |Parses a character into the proper function.
 parse_char :: Word32 -> ProgState -> IO ProgState
 parse_char c st
-    | c == (toEnum . fromEnum) '>' = ptr_right st
-    | c == (toEnum . fromEnum) '<' = ptr_left st
-    | c == (toEnum . fromEnum) '+' = val_incr st
-    | c == (toEnum . fromEnum) '-' = val_decr st
+    | c == (toEnum . fromEnum) '>' = return $ ptr_right st
+    | c == (toEnum . fromEnum) '<' = return $ ptr_left st
+    | c == (toEnum . fromEnum) '+' = return $ val_incr st
+    | c == (toEnum . fromEnum) '-' = return $ val_decr st
     | c == (toEnum . fromEnum) '.' = val_prnt st
     | c == (toEnum . fromEnum) ',' = val_inpt st
     | c == (toEnum . fromEnum) '[' = if (vals st)!(ptr st) /= 0
-                                       then beg_loop st
-                                       else jmp_loop st
-    | c == (toEnum . fromEnum) ']' = end_loop st
-    | otherwise = nul_func st
+                                       then return $ beg_loop st
+                                       else return $ jmp_loop st
+    | c == (toEnum . fromEnum) ']' = return $ end_loop st
+    | otherwise = return $ nul_func st
 
 
--- Moves the current array pointer right by 1 index.
-ptr_right :: ProgState -> IO ProgState
-ptr_right st = return (ProgState (ptr st + 1) (vals st) (cnsm_file_char st) 
-               (loop_fl st))
+-- |Moves the current array pointer right by 1 index.
+ptr_right :: ProgState -> ProgState
+ptr_right st = st { ptr = ptr st + 1
+                  , file = cnsm_file_char st
+                  }
 
--- Moves the current array pointer left by 1 index.
-ptr_left :: ProgState -> IO ProgState
-ptr_left st = return (ProgState (ptr st - 1) (vals st) (cnsm_file_char st) 
-              (loop_fl st))
+-- |Moves the current array pointer left by 1 index.
+ptr_left :: ProgState -> ProgState
+ptr_left st = st { ptr = ptr st - 1
+                 , file = cnsm_file_char st
+                 }
 
--- Increments the value at the current pointer by one.
-val_incr :: ProgState -> IO ProgState
-val_incr st = return (ProgState (ptr st) 
-              ((vals st)//[(ptr st,succ $ (vals st)!(ptr st))]) 
-              (cnsm_file_char st) (loop_fl st))
+-- |Increments the value at the current pointer by one.
+val_incr :: ProgState -> ProgState
+val_incr st = st { vals = vals st//[(ptr st,succ $ (vals st)!(ptr st))]
+                 , file = cnsm_file_char st
+                 }
 
--- Decrements the value at the current pointer by one.
-val_decr :: ProgState -> IO ProgState
-val_decr st = return (ProgState (ptr st) 
-              ((vals st)//[(ptr st,pred $ (vals st)!(ptr st))]) 
-              (cnsm_file_char st) (loop_fl st))
+-- |Decrements the value at the current pointer by one.
+val_decr :: ProgState -> ProgState
+val_decr st = st { vals = vals st//[(ptr st,pred $ (vals st)!(ptr st))]
+                 , file = cnsm_file_char st
+                 }
 
--- Prints the Word32 at vals!ptr.
+-- |Prints the Word32 at vals!ptr.
 val_prnt :: ProgState -> IO ProgState
-val_prnt st = putChar ((toEnum . fromEnum) ((vals st)!(ptr st))) >> return 
-              (ProgState (ptr st) (vals st) (cnsm_file_char st) (loop_fl st))
+val_prnt st = putChar ((toEnum . fromEnum) ((vals st)!(ptr st))) >> return
+              st { file = cnsm_file_char st }
 
--- Reads a Word32 from the user to put at vals!ptr.
+-- |Reads a Word32 from the user to put at vals!ptr.
 val_inpt :: ProgState -> IO ProgState
-val_inpt st = do
-    c <- getChar
-    return (ProgState (ptr st) ((vals st)//[(ptr st,(toEnum . fromEnum) c)]) 
-                          (cnsm_file_char st) (loop_fl st))
+val_inpt st = getChar >>= \c
+            -> return st { vals = vals st//[(ptr st,(toEnum . fromEnum) c)]
+                         , file = cnsm_file_char st
+                         }
 
--- Consumes one character but otherwise has no affect on the ProgState.
-nul_func :: ProgState -> IO ProgState
-nul_func st = return 
-              (ProgState (ptr st) (vals st) (cnsm_file_char st) (loop_fl st))
 
--- Begins a loop.
-beg_loop :: ProgState -> IO ProgState
+-- |Consumes one character but otherwise has no affect on the ProgState.
+nul_func :: ProgState -> ProgState
+nul_func st = st { file = cnsm_file_char st }
+
+-- |Begins a loop.
+beg_loop :: ProgState -> ProgState
 beg_loop st = if (vals st)!(ptr st) == 0
                 then jmp_loop st
-                else return (ProgState (ptr st) (vals st) (cnsm_file_char st)
-                                       ((cnsm_file_char st):loop_fl st))
+                else st { file    = cnsm_file_char st
+                        , loop_fl = cnsm_file_char st:loop_fl st
+                        }
 
--- Brings the program back to the top of a loop if the value at the ptr is /= 0.
-end_loop :: ProgState -> IO ProgState
+-- |Brings the program back to the top of a loop if the value at the ptr is /= 0.
+end_loop :: ProgState -> ProgState
 end_loop st = if (vals st)!(ptr st) == 0
-                then return (ProgState (ptr st) (vals st) (cnsm_file_char st) 
-                                           (tail (loop_fl st)))
-                else return (ProgState (ptr st) (vals st) (head (loop_fl st))
-                                       (loop_fl st))
-                
--- Jumps to the end of a loop without processing the contents.
-jmp_loop :: ProgState -> IO ProgState
-jmp_loop st = do
-    let c = (head . snd . file) st
-    if c == (toEnum . fromEnum) ']'
-      then end_loop st
-      else nul_func st >>= jmp_loop
+                then st { file    = cnsm_file_char st
+                        , loop_fl = tail $ loop_fl st
+                        }
+                else st { file    = head $ loop_fl st }
 
--- Moves one char from the left to the consumed.
+-- Jumps to the end of a loop without processing the contents.
+jmp_loop :: ProgState -> ProgState
+jmp_loop st = if (head . snd . file) st == (toEnum . fromEnum) ']'
+                then end_loop st
+                else jmp_loop $ nul_func st
+
+-- |Moves one char from the left to the consumed.
 cnsm_file_char :: ProgState -> (String,String)
-cnsm_file_char st = let f = file st in (fst f ++ [head (snd f)],tail (snd f))
+cnsm_file_char st = let f = file st
+                    in (fst f ++ [head $ snd f],tail $ snd f)
