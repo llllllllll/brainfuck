389e9919cf04ea499467bb96631f05baca419c9d
llllllllll <joejev@gmail.com>
2013-12-16 22:29:57 -0500

    changed it to an array of Commands to iterate over.

--- src/interpreter.hs
+++ src/interpreter.hs
@@ -1,142 +1,204 @@
 -- Joe Jevnik
 -- 9.11.2013
--- Edited: 13.10.2013
+-- Edited: 16.12.2013
 
 import Control.Applicative ((<$>))
-import Control.Monad      (void)
-import Data.Array.Unboxed (UArray,(!),(//),listArray)
-import Data.Word          (Word32)
-import System.Environment (getArgs)
-import Text.Read          (readMaybe)
+import Control.Monad       (when)
+import Data.Array.Unboxed  (Array,UArray,(!),(//),listArray,bounds)
+import Data.List           (groupBy)
+import Data.Word           (Word8)
+import System.Environment  (getArgs)
+import System.Exit         (exitSuccess)
+import Text.Read           (readMaybe)
 
--- Data type to hold the state of the program.
-data ProgState = ProgState { ptr        :: !Int              -- Value Pointer
-                           , vals       :: UArray Int Word32  -- Values
-                           , state      :: (String,String)   -- (consumed,left)
-                           , loopStates :: [(String,String)] -- (consumed,left)
+-- -----------------------------------------------------------------------------
+-- Data types.
+
+-- | Represents the types of commands that can be read or performed.
+data Command = PtrRight       -- ^ '>'
+              | PtrLeft       -- ^ '<'
+              | PtrJump Int   -- ^ repeated calls to ptr movements.
+              | ValIncr       -- ^ '+'
+              | ValDecr       -- ^ '-'
+              | ValChange Int -- ^ repeted cals to val changes
+              | ValPrnt       -- ^ '.'
+              | ValInpt       -- ^ ','
+              | BegLoop       -- ^ '['
+              | EndLoop       -- ^ ']'
+              | NulFunc       -- ^ Do nothing.
+                deriving (Eq,Show)
+
+-- | Data type to hold the state of the program.
+data ProgState = ProgState { ptr        :: Int
+                           , tape       :: UArray Int Word8
+                           , currCmd    :: Int
+                           , cmds       :: Array Int Command
+                           , loopStates :: [Int]
                            }
+                 deriving Show
 
--- |Starts consuming characters from the ProgState file.
+-- -----------------------------------------------------------------------------
+-- File Parsing and reading.
+
 main :: IO ()
-main = void $ getArgs >>= parseArgs >>= consumeChars
+main = getArgs >>= parseArgs
 
--- Parses the command line arguments to interperet the flags.
-parseArgs :: [String]-> IO ProgState
-parseArgs as =
-    if head as == "-m"
-      then do
-          fl <- readFile $ as!!2
-          let ln=  case readMaybe $ as!!1 of
-                       Nothing -> error "'-m' expects and Int argument"
-                       Just n  -> n - 1
-          return ProgState { ptr        = 0
-                           , vals       = listArray (0,ln) (repeat 0)
-                           , state      = ("",fl)
-                           , loopStates = []
-                           }
-      else do
-          fl <- readFile $ head as
-          return ProgState { ptr        = 0
-                           , vals       = listArray (0,29999) (repeat 0)
-                           , state      = ("",fl)
-                           , loopStates = []
-                           }
+-- | Parses the args and starts interpreting.
+parseArgs :: [String] -> IO ()
+parseArgs [] = putStrLn "Usage: runbrainfuck [FILE]"
+parseArgs as = do
+    let b = "-O2" `elem` as
+    cs <- readFile (if b
+                      then (head . tail) as
+                      else head as)
+          >>= return . (if b
+                          then o2
+                          else removeComments) . getCommands
+    process $ ProgState { ptr        = 0
+                        , tape       = listArray (0,29999) $ repeat 0
+                        , currCmd    = 0
+                        , cmds       = listArray (0,length cs - 1) cs
+                        , loopStates = []
+                        }
 
--- |Parses the next character from the ProgState.
-consumeChars :: ProgState -> IO ProgState
-consumeChars st =
-    if (not $ null ((snd . state) st))
-      then parseChar ((toEnum . fromEnum) $ (head . snd . state) st) st
-               >>= consumeChars
-      else return (ProgState 0 (listArray (0,29999) (repeat 0)) ("","") [])
+-- | Reads a string into a list of 'Command's
+getCommands :: String -> [Command]
+getCommands =  map parseChar
+  where
+      parseChar c
+          | c == '>'  = PtrRight
+          | c == '<'  = PtrLeft
+          | c == '+'  = ValIncr
+          | c == '-'  = ValDecr
+          | c == '.'  = ValPrnt
+          | c == ','  = ValInpt
+          | c == '['  = BegLoop
+          | c == ']'  = EndLoop
+          | otherwise = NulFunc
 
--- |Parses a character into the proper function.
-parseChar :: Word32 -> ProgState -> IO ProgState
-parseChar c st
-    | c == charToWord '>' = return $ ptrRight st
-    | c == charToWord '<' = return $ ptrLeft st
-    | c == charToWord '+' = return $ valIncr st
-    | c == charToWord '-' = return $ valDecr st
-    | c == charToWord '.' = valPrnt st
-    | c == charToWord ',' = valInpt st
-    | c == charToWord '[' = if (vals st)!(ptr st) /= 0
-                              then return $ begLoop st
-                              else return $ jmpLoop st
-    | c == charToWord ']' = return $ endLoop st
-    | otherwise = return $ nulFunc st
+-- | Converts a 'Command' into the proper function
+parseCmd :: Command -> ProgState -> IO ProgState
+parseCmd PtrRight      = return . ptrJump     1
+parseCmd PtrLeft       = return . ptrJump   (-1)
+parseCmd (PtrJump n)   = return . ptrJump     n
+parseCmd ValIncr       = return . valChange   1
+parseCmd ValDecr       = return . valChange (-1)
+parseCmd (ValChange n) = return . valChange   n
+parseCmd ValPrnt       = valPrnt
+parseCmd ValInpt       = valInpt
+parseCmd BegLoop       = return . begLoop
+parseCmd EndLoop       = return . endLoop
+parseCmd NulFunc       = return . nulFunc
 
+process :: ProgState -> IO ()
+process st
+    | currCmd st > (snd . bounds) (cmds st) = return ()
+    | otherwise                             = (applyCommand st >>= process)
 
--- | Moves the current array pointer right by 1 index.
-ptrRight :: ProgState -> ProgState
-ptrRight st = st { ptr = ptr st + 1
-                 , state = consumeChar st
-                 }
+-- | Applies the current command to the state.
+applyCommand :: ProgState -> IO ProgState
+applyCommand st = parseCmd (cmds st ! currCmd st) $ st
 
--- | Moves the current array pointer left by 1 index.
-ptrLeft :: ProgState -> ProgState
-ptrLeft st = st { ptr = ptr st - 1
-                , state = consumeChar st
-                }
+-- -----------------------------------------------------------------------------
+-- Optimization.
 
--- | Increments the value at the current pointer by one.
-valIncr :: ProgState -> ProgState
-valIncr st = st { vals = vals st//[(ptr st,succ $ (vals st)!(ptr st))]
-                , state = consumeChar st
-                }
+-- | Removes all the Comments from the list of commands to speed up
+-- interpretation.
+removeComments :: [Command] -> [Command]
+removeComments = filter (/= NulFunc)
 
--- | Decrements the value at the current pointer by one.
-valDecr :: ProgState -> ProgState
-valDecr st = st { vals = vals st//[(ptr st,pred $ (vals st)!(ptr st))]
-                , state = consumeChar st
-                }
+-- | Collapses all chains of '+' and '-' into a single ValChange command.
+collapseValChanges :: [Command] -> [Command]
+collapseValChanges cs = let cs' = groupBy g cs
+                        in cs' >>= \c -> case count c of
+                                             (0,0)   -> c
+                                             (ic,dc) -> [ValChange (ic - dc)]
+  where
+      g ValIncr ValIncr  = True
+      g ValIncr ValDecr  = True
+      g ValDecr ValIncr  = True
+      g ValDecr ValDecr  = True
+      g _ _              = False
+      count []           = (0,0)
+      count (ValIncr:cs) = (\(a,b) -> (a + 1,b)) $ count cs
+      count (ValDecr:cs) = (\(a,b) -> (a,b + 1)) $ count cs
+      count (c:cs)       = count cs
 
--- | Prints the Word32 at vals!ptr.
+-- | Collapses all chains of '>' and '<' into one PtrJump command.
+collapsePtrJmps :: [Command] -> [Command]
+collapsePtrJmps cs = let cs' = groupBy g cs
+                     in cs' >>= \c -> case count c of
+                                          (0,0)   -> c
+                                          (rc,lc) -> [PtrJump (rc - lc)]
+  where
+      g PtrRight PtrRight = True
+      g PtrRight PtrLeft  = True
+      g PtrLeft  PtrRight = True
+      g PtrLeft  PtrLeft  = True
+      g _ _               = False
+      count []            = (0,0)
+      count (PtrRight:cs) = (\(a,b) -> (a + 1,b)) $ count cs
+      count (PtrLeft:cs)  = (\(a,b) -> (a,b + 1)) $ count cs
+      count (c:cs)        = count cs
+
+-- | Applies the full optimizations.
+o2 :: [Command] -> [Command]
+o2 = collapsePtrJmps . collapseValChanges . removeComments
+
+-- -----------------------------------------------------------------------------
+-- Commands.
+
+-- | Moves the current array pointer to the right by n.
+ptrJump :: Int -> ProgState -> ProgState
+ptrJump n st = case ptr st + n of
+                   m | m > 29999 -> error "Moved off the right side of the tape"
+                     | m < 0     -> error "Moved off the left side of the tape"
+                     | otherwise -> nulFunc st { ptr = m }
+
+-- | Increments the value at the current pointer by n.
+valChange :: Int -> ProgState -> ProgState
+valChange n st = nulFunc st { tape = tape st//[(ptr st,change n st)] }
+  where
+      change n st = fromIntegral $ n + fromIntegral (tape st ! ptr st)
+
+-- | Prints the Word8 at tape st ! ptr st.
 valPrnt :: ProgState -> IO ProgState
-valPrnt st = putChar ((toEnum . fromEnum) ((vals st)!(ptr st))) >> return
-              st { state = consumeChar st }
+valPrnt st = putChar ((toEnum . fromEnum) (tape st ! ptr st ))
+             >> return (nulFunc st)
 
--- | Reads a Word32 from the user to put at vals!ptr.
+-- | Reads a Word8 from the user to put at tape!ptr.
 valInpt :: ProgState -> IO ProgState
-valInpt st = getChar >>= \c
-            -> return st { vals = vals st//[(ptr st,(toEnum . fromEnum) c)]
-                         , state = consumeChar st
-                         }
+valInpt st = getChar >>= \c -> return $ nulFunc
+             st { tape = tape st//[(ptr st,(toEnum . fromEnum) c)] }
 
 -- | Consumes one character but otherwise has no affect on the ProgState.
 nulFunc :: ProgState -> ProgState
-nulFunc st = st { state = consumeChar st }
+nulFunc st = st { currCmd = currCmd st + 1 }
 
 -- | Begins a loop.
 begLoop :: ProgState -> ProgState
-begLoop st = if (vals st)!(ptr st) == 0
-                then jmpLoop st
-                else st { state    = consumeChar st
-                        , loopStates = consumeChar st:loopStates st
-                        }
+begLoop st
+    | tape st ! ptr st == 0 = jmpLoop st
+    | otherwise             = nulFunc st { loopStates = currCmd st + 1
+                                                        : loopStates st }
 
 -- | Brings the program back to the top of a loop if the value at the
 -- ptr is /= 0.
 endLoop :: ProgState -> ProgState
-endLoop st = if (vals st)!(ptr st) == 0
-                then st { state    = consumeChar st
-                        , loopStates = tail $ loopStates st
-                        }
-                else st { state    = head $ loopStates st }
+endLoop st
+    | tape st ! ptr st == 0 = nulFunc st { loopStates = drop 1 $ loopStates st }
+    | otherwise             = st { currCmd = head $ loopStates st }
 
 -- | Jumps to the end of a loop without processing the contents.
 jmpLoop :: ProgState -> ProgState
-jmpLoop st = if (head . snd . state) st == (toEnum . fromEnum) ']'
-                then endLoop st
-                else jmpLoop $ nulFunc st
+jmpLoop st
+    | cmds st ! (currCmd st + 1) == EndLoop = endLoop st
+    | otherwise                             = jmpLoop $ nulFunc st
+
 
 -- -----------------------------------------------------------------------------
 -- Helper functions
 
--- | Moves one char from the left to the consumed.
-consumeChar :: ProgState -> (String,String)
-consumeChar st = let f = state st
-                    in (fst f ++ [head $ snd f],tail $ snd f)
-
--- | Converts a Char to a Word32
-charToWord :: Char -> Word32
+-- | Converts a Char to a Word8
+charToWord :: Char -> Word8
 charToWord = toEnum . fromEnum
